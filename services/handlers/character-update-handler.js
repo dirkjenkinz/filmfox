871511aa43b2a0d75e1b4d87316e40cc0183d1f3
dirk jenkinz <dirkjenkinz@gmail.com>
2023-07-17 15:25:53 +0100

    voice mapping

--- services/handlers/character-update-handler.js
+++ services/handlers/character-update-handler.js
@@ -1,16 +1,25 @@
 
 const url = require('url');
-const {getData} = require('../file-service');
+const { getData, writeFile } = require('../file-service');
 
 const characterUpdateHandler = async (req, res) => {
   console.log('entering character update handler');
 
   let u = url.parse(req.originalUrl, true);
+  let voice = u.query.voice;
+  let character = u.query.character;
   let file = u.query.filmFoxFile;
-  console.log({file});
-  let filmFoxFile = await getData(file+'.fff');
+  let fff = await getData(file + '.fff');
 
-  const { title, characters, voice_data} = filmFoxFile;
+  const { title, characters, voice_data } = fff;
+
+  characters.forEach(c => {
+    if (c[0] === character) {
+      c[1] = voice;
+    }
+  })
+
+  writeFile(JSON.stringify(fff), title + '.fff');
 
   res.render('character-to-voice.njk', {
     title,
