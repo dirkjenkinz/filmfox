dc8023ff5fdaffd24f518da957274eb25f67d55a
dirk jenkinz <dirkjenkinz@gmail.com>
2023-07-18 11:55:19 +0100

    disable gen if no voice

--- services/elevenLabs.js
+++ services/elevenLabs.js
@@ -5,10 +5,6 @@ const directoryPath = path.join(__dirname, '../data');
 
 const  generateSpeech = (apiKey, voiceID, fileName, textInput) => {
     console.log('generate speech');
-    console.log({apiKey});
-    console.log({voiceID})
-    console.log({fileName});
-    console.log({textInput});
     voice.textToSpeech(apiKey, voiceID, `${directoryPath}/${fileName}`, textInput).then(res => {
         console.log(res);
     })
