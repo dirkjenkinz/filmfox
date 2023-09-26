4eb676ad6dea6fc5ec9711e2c9690c2e151d1b4c
patrick whittaker <patrick.whittaker@live.com>
2023-09-26 11:05:56 +0100

    parent

--- js/character-to-voice.js
+++ js/character-to-voice.js
@@ -1,20 +1,38 @@
-$("#display").on("click", () => {
-    const fff = $("#title")[0].outerText
-    let sceneNumber = $("#sceneNumber")[0].innerText
-    window.location.href = `/display?filmFoxFile=${fff}.fff&sceneNumber=${sceneNumber}`
+$("#front").on("click", () => {
+  window.location.href = `/`;
+}),
+  $("#btn-build-showreel").on("click", () => {
+    const f = $("#title")[0].innerText;
+    const sceneNumber = $("#sceneNumber")[0].innerText;
+    window.location.href = `/build-showreel?title=${f}&sceneNumber=${sceneNumber}`;
   }),
-  $('#btn-back').on('click', () => {
-    const title = $('#title')[0].innerText;
-    const sceneNumber = $('#sceneNumber')[0].innerText;
-    window.location.href = `/display?title=${title}&sceneNumber=${sceneNumber}`;
+  $("#btn-characters").on("click", () => {
+    const title = $("#title")[0].innerText;
+    const sceneNumber = $("#sceneNumber")[0].innerText;
+    window.location.href = `/characters?title=${title}&sceneNumber=${sceneNumber}`;
   }),
-    $("select").on("change", function (e) {
-      const num = this.name.substring(7)
-      const id = this.id
-      const cid = `char_${num}`
-      const voice = $(`#${id} option:selected`).text().trim()
-      const character = $(`#${cid}`)[0].textContent
-      const title = $("#title")[0].outerText
-      let sceneNumber = $("#sceneNumber")[0].innerText
-      window.location.href = `/character-update?filmFoxFile=${title}&character=${character}&voice=${voice}&sceneNumber=${sceneNumber}`
-    })
+  $("#btn-merge").on("click", () => {
+    const f = $("#title")[0].innerText;
+    const sceneNumber = $("#sceneNumber")[0].innerText;
+    window.location.href = `/merge?title=${f}&sceneNumber=${sceneNumber}`;
+  }),
+  $("#btn-scenes").on("click", () => {
+    const title = $("#title")[0].innerText;
+    const sceneNumber = $("#sceneNumber")[0].innerText;
+    window.location.href = `/scenes?title=${title}&sceneNumber=${sceneNumber}`;
+  }),
+  $("#btn-video").on("click", () => {
+    const title = $("#title")[0].innerText;
+    const sceneNumber = $("#sceneNumber")[0].innerText;
+    window.location.href = `/video?title=${title}&sceneNumber=${sceneNumber}`;
+  }),
+  $("select").on("change", function (e) {
+    const num = this.name.substring(7);
+    const id = this.id;
+    const cid = `char_${num}`;
+    const voice = $(`#${id} option:selected`).text().trim();
+    const character = $(`#${cid}`)[0].textContent;
+    const title = $("#title")[0].outerText;
+    let sceneNumber = $("#sceneNumber")[0].innerText;
+    window.location.href = `/character-update?filmFoxFile=${title}&character=${character}&voice=${voice}&sceneNumber=${sceneNumber}`;
+  });
