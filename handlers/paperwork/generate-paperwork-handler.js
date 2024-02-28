805f200a593089ffa0a5135c9eaf455228f145f5
patrick whittaker <patrick.whittaker@live.com>
2024-02-28 10:07:13 +0000

    paperwork layout

--- handlers/paperwork/generate-paperwork-handler.js
+++ handlers/paperwork/generate-paperwork-handler.js
@@ -11,16 +11,17 @@ const generatePaperworkHandler = async (req, res) => {
   // Parse the URL to extract query parameters
   const u = url.parse(req.originalUrl, true);
   const title = u.query.title;
+  let categoryDirectory = path.join(__dirname, `../../data/${title}/paperwork/breakdown`);
 
   // Render the categories template with necessary data
   res.render('generate-paperwork.njk', {
     title,
     page: 'Generate Paperwork',
     caller: 'generate-paperwork',
-    categoryDirectory: path.join(__dirname, `../../data/${title}/paperwork/breakdown`),
-    sheetDirectory: path.join(__dirname, `../../data/${title}/paperwork/sheets`),
-    shotDirectory: path.join(__dirname, `../../data/${title}/paperwork/shots`),
-    powerpointDirectory: path.join(__dirname, `../../data/${title}/paperwork/ppt`),
+    categoryDirectory: `.../${title}/paperwork/breakdown`,
+    sheetDirectory: `.../${title}/paperwork/sheets`,
+    shotDirectory: `.../${title}/paperwork/shots`,
+    powerpointDirectory: `.../${title}/paperwork/ppt`,
   });
 };
 
