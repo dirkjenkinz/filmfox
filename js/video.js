ab958464a3e3cbbb013477119884f075d8dcda8f
patrick whittaker <patrick.whittaker@live.com>
2024-03-12 11:48:22 +0000

    cosmetic stuff mostly

--- js/video.js
+++ js/video.js
@@ -1,32 +1,36 @@
+// Wait for the document to be ready before executing the code
 $(document).ready(() => {
+  // Set the 'active' class for the 'nav-video' element
   $('#nav-video').addClass('active');
 
-  $('.btn-generate-elements').on('click', (e) => {
+  // Function to generate elements based on film title and scene number
+  const generateElements = (sceneNumber) => {
     const title = $('#filmTitle')[0].innerText;
-    const sceneNumber = e.target.value;
+    // Redirect to the create-video page with parameters
     window.location.href = `/create-video?title=${title}&sceneNumber=${sceneNumber}`;
+  };
+
+  // Event handler for 'btn-generate-elements' click
+  $('.btn-generate-elements').on('click', (e) => {
+    generateElements(e.target.value);
   });
 
+  // Event handler for 'btn-generate-scene' click
   $('.btn-generate-scene').on('click', (e) => {
-    const title = $('#filmTitle')[0].innerText;
-    const sceneNumber = e.target.value;
-    window.location.href = `/generate-scene?title=${title}&sceneNumber=${sceneNumber}`;
+    generateElements(e.target.value);
   });
 
+  // Event handler for 'btn-generate-all' click
   $('#btn-generate-all').on('click', () => {
     const title = $('#filmTitle')[0].innerText;
+    // Redirect to generate-scene page with 'all' as the scene number
     window.location.href = `/generate-scene?title=${title}&sceneNumber=all`;
   });
 
+  // Event handler for 'btn-concatenate-video' click
   $('#btn-concatenate-video').on('click', () => {
     const title = $('#filmTitle')[0].innerText;
+    // Redirect to concatenate-video page with the film title as a parameter
     window.location.href = `/concatenate-video?title=${title}`;
-  });
-
-  $('#btn-generate-powerpoint').on('click', () => {
-    const title = $('#filmTitle')[0].innerText;
-    const sceneNumber = $('#sceneNumber')[0].innerText;
-    const elementNumber = $('#elementNumber')[0].innerText;
-    window.location.href = `/generate-powerpoint?title=${title}&sceneNumber=${sceneNumber}&elementNumber=${elementNumber}`;
   });
 });
